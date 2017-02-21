84d2da1652c83444c802e07292155fd4c732991c
mdiblasio <mdiblasio@google.com>
2017-02-21 14:30:52 -0500

    Exercise 2

--- app/src/main/java/com/google/gtech/pubs/androidsdktraining/MainActivity.java
+++ app/src/main/java/com/google/gtech/pubs/androidsdktraining/MainActivity.java
@@ -2,6 +2,7 @@ package com.google.gtech.pubs.androidsdktraining;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.FrameLayout;
 import android.widget.ImageView;
 import android.widget.TextView;
@@ -23,16 +24,15 @@ public class MainActivity extends AppCompatActivity {
         pictureFrame = (FrameLayout) findViewById(R.id.pictureFrame);
         title = (TextView) findViewById(R.id.titleTV);
         office = (TextView) findViewById(R.id.officeTV);
-
-        populateViews();
     }
 
     /**
      * Method to populate text views in profile layout and create
      * and attach and ImageView view object to the 'pictureFrame'
-     * FrameLayout
+     * FrameLayout.
+     * Called when button is clicked from profile layout.
      */
-    void populateViews() {
+    void populateViews(View v) {
         // set text from defined strings
         name.setText(R.string.name);
         title.setText(R.string.title);
